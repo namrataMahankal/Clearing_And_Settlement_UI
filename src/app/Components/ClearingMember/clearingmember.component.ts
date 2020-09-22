5d67c7dd572fba21446101ee15aa11538bf1cdd8
namrataMahankal <namrata1997@gmail.com>
2020-09-22 11:48:32 +0530

    angular installed

---
+++ src/app/Components/ClearingMember/clearingmember.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clearing-member',
+  templateUrl: './clearingmember.component.html',
+ // styleUrls: ['./clearingmember.component.css']
+})
+export class ClearingMemberComponent {
+  title = 'Clearing-And-Settlement-UI';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClearingMemberComponent } from './Components/ClearingMember/clearingmember.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClearingMemberComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
