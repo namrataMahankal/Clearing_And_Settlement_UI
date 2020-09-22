22267e1c704c37fbe9445a2e38a9190f47378522
namrataMahankal <namrata1997@gmail.com>
2020-09-22 18:47:20 +0530

    corp actions

--- src/app/Components/ClearingMember/clearingmember.component.ts
+++ src/app/Components/ClearingMember/clearingmember.component.ts
@@ -7,11 +7,14 @@ import { Component } from '@angular/core';
  // styleUrls: ['./clearingmember.component.css']
 })
 export class ClearingMemberComponent {
+  shortage:Boolean=true;
   title = 'Clearing-And-Settlement-UI';
   displayedColumns: string[] = ['BuyerCM', 'SellerCM', 'ES', 'Qty','Price','TradeValue'];
   displayedColumns1: string[] = ['Securities', 'Shares'];
+  displayedColumnsCorpActions: string[] = ['Securities','Actions','Initial fund balance','Initial share balance','Current fund balance','Current share balance'];
   dataSource = Trade_list;
   dataSource1= security_share_list;
+  dataSourceCorpActions=CorpActions_list;
 }
 
 export interface TradeListElement {
@@ -22,15 +25,31 @@ export interface TradeListElement {
   Price: number;
   TradeValue:number;
 }
-
+export interface CorpActionsList {
+    Securities: string;
+    Action: string;
+    Initial_fund_bal: number;
+    Initial_share_bal: number;
+    Current_fund_bal: number;
+    Current_share_bal:number;
+  }
+  
 export interface SecuritySharesListElement {
   Securities: string;
   Shares:number;
 }
 
+
 const Trade_list: TradeListElement[] = [
   {BuyerCM: "Citi", SellerCM: 'GS', ES: 'Apple', Qty: 100,Price: 12,TradeValue:100 },
   {BuyerCM: "Citi", SellerCM: 'GS', ES: 'Apple', Qty: 100,Price: 12,TradeValue:100 },
+];
+
+const CorpActions_list: CorpActionsList[] = [
+    {Securities: "Amazon", Action: 'Stock Split', Initial_fund_bal: 10000 ,Initial_share_bal: 1987,Current_fund_bal: 180000,Current_share_bal:2890},
+    {Securities: "Apple", Action: 'Stock Dividend', Initial_fund_bal: 10000 ,Initial_share_bal: 1987,Current_fund_bal: 180000,Current_share_bal:2890},
+    {Securities: "Google", Action: 'Rights', Initial_fund_bal: 10000 ,Initial_share_bal: 1987,Current_fund_bal: 180000,Current_share_bal:2890},
+    {Securities: "Amazon", Action: 'Stock Split', Initial_fund_bal: 10000 ,Initial_share_bal: 1987,Current_fund_bal: 180000,Current_share_bal:2890},
 ];
 
 const security_share_list: SecuritySharesListElement[]=[
