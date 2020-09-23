7086324b0aae062c3f0c6a5b07e360b8579494fb
Justin Jose <36569747+AnthonySoprano@users.noreply.github.com>
2020-09-23 18:52:26 +0530

    Revert "Merge branch 'ch'"
    This reverts commit 9fefba77efc03903e2c4dc25cf42a427c8510fd2, reversing
    changes made to a24421b25265458c65ef614c396947b3a79c96de.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClearingMemberComponent } from './Components/ClearingMember/clearingmember.component';
-import { ClearingHouseComponent } from './Components/ClearingHouse/clearinghouse.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatListModule} from '@angular/material/list';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ClearingMemberComponent,
-    ClearingHouseComponent
+    ClearingMemberComponent
   ],
   imports: [
     BrowserModule,
