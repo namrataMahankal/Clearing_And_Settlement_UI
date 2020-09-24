dbe8a81ad2ae491316d656851a16c2606e83f23d
Akshaj <akshaj@gmail.com>
2020-09-24 15:35:18 +0530

    Admin Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClearingMemberComponent } from './Components/ClearingMember/clearingmember.component';
 import { ClearingHouseComponent } from './Components/ClearingHouse/clearinghouse.component';
+import {AdminComponent}   from './Components/Admin/admin.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     ClearingMemberComponent,
-    ClearingHouseComponent
+    ClearingHouseComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +32,12 @@ import {MatListModule} from '@angular/material/list';
     MatToolbarModule,
     MatTableModule,
     MatSlideToggleModule,
-    MatListModule
+    MatListModule,
+    MatExpansionModule,
+    MatInputModule,
+   // FormBuilder,
+   ReactiveFormsModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
