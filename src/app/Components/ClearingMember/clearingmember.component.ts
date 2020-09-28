1996245e33280c2506690d4f38d4ff4521108e43
Justin Jose <36569747+AnthonySoprano@users.noreply.github.com>
2020-09-28 12:16:20 +0530

    Revert "Merge pull request #8 from namrataMahankal/ch"
    This reverts commit 17c756ecb4500130f267a0cc1a14c13ca253d6fc, reversing
    changes made to d8b0f9f0a1a76781e19cafaf6c23e19c2ac9095e.

--- src/app/Components/ClearingMember/clearingmember.component.ts
+++ src/app/Components/ClearingMember/clearingmember.component.ts
@@ -13,14 +13,8 @@ export class ClearingMemberComponent {
   shortage:Boolean=true;
   obCnt=5;
   title = 'Clearing-And-Settlement-UI';
-<<<<<<< HEAD
-  displayedColumns: string[] = ['BuyerCM', 'SellerCM', 'ES', 'Qty','Price','TradeValue'];
-  displayedColumns1: string[] = ['Securities','Opening_Balance','Closing_Balance', 'Shares_Obliged','Status'];
-  displayedColumns2:string[]=['Securities','Shares','Price','Amount'];
-=======
   displayedColumns: string[] = ['ES', 'Qty','Price','TradeValue'];
   displayedColumns1: string[] = ['Securities', 'Shares','Status'];
->>>>>>> d8b0f9f0a1a76781e19cafaf6c23e19c2ac9095e
   displayedColumnsProfile: string[] = ['Securities', 'Shares'];
   displayedColumnsCorpActions: string[] = ['Securities','Actions','Parameter','Initial share balance','Corp Action effect','Current share balance'];
   displayedColumnsSettlement: string[] = ['Securities', 'Shares', 'Rate', 'Cost'];
@@ -28,11 +22,6 @@ export class ClearingMemberComponent {
   dataSource1= security_share_list;
   dataSourceCorpActions=CorpActions_list;
   dataSourceProfile=Profile_list;
-<<<<<<< HEAD
-  dataSettlement = SettlementList;
-  dataSourceFunds=DayFundsChange;
-  
-=======
   dataSettlement : SettlementElement[] = [
     { Securities: 'Apple', Shares: 100, Rate: 1.23, Cost: 1234 },
     { Securities: 'Amazon', Shares: 100, Rate: 1.23, Cost: 1234 },
@@ -43,8 +32,8 @@ export class ClearingMemberComponent {
   getTotalCost() {
     return this.dataSettlement.map(t => t.Cost).reduce((acc, value) => acc + value, 0);
   }
->>>>>>> d8b0f9f0a1a76781e19cafaf6c23e19c2ac9095e
 }
+
 export interface TradeListElement {
   ES: string;
   Qty: number;
@@ -62,18 +51,8 @@ export interface CorpActionsList {
   
 export interface SecuritySharesListElement {
   Securities: string;
-  Opening_Balance:number;
-  Closing_Balance:number;
-  Shares_Obliged: number;
-  Status: String;
-
-}
-
-export interface FundsElement{
-  Securities: String;
-  Shares :number;
-  Price:number;
-  Amount:number;
+  Shares:number;
+  Status:string;
 }
 
 export interface ProfileElement {
@@ -83,12 +62,11 @@ export interface ProfileElement {
 }
 
 export interface SettlementElement {
-  Securities: String;
+  Securities: string;
   Shares: number;
   Rate: number;
   Cost: number;
 }
-
 
 
 const Trade_list: TradeListElement[] = [
@@ -106,27 +84,18 @@ const CorpActions_list: CorpActionsList[] = [
 ];
 
 const security_share_list: SecuritySharesListElement[]=[
-
-  {Securities:'Apple',Opening_Balance:10000,Closing_Balance:10000, Shares_Obliged: -6000,Status:"No Shortage"},
-  {Securities:'Amazon',Opening_Balance:200,Closing_Balance:500, Shares_Obliged: -1000,Status:"Shortage"},
-
+ {Securities:'Apple',Shares:100,Status:'To be received'},
+ {Securities:'Amazon',Shares:300,Status:'To be sent'},
+ {Securities:'Google',Shares:100,Status:'To be received'},
+ {Securities:'Amazon',Shares:300,Status:'To be sent'},
 
 ];
-
 
 const Profile_list: ProfileElement[] = [
   {Securities:'Apple',Shares:100},
   {Securities:'Amazon',Shares:300},
 ];
 
-
-
-const DayFundsChange:FundsElement[]=[  
-  {Securities: 'Amazon',Shares :200,Price:10, Amount:-2000},
-  {Securities: 'Google',Shares :200,Price:10, Amount:-2000},
-  {Securities: 'Apple',Shares :200,Price:10, Amount:-2000}
-
-];
 
 
 export class SlideToggleOverviewExample {}
