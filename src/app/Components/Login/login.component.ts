b23f9df201e329296dec9770cc2f23302a1efc63
Akshaj <akshaj@gmail.com>
2020-09-30 16:26:51 +0530

    integration for authentication

--- src/app/Components/Login/login.component.ts
+++ src/app/Components/Login/login.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import {  FormGroup,FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../Service/authentication.service';
+import { AuthenticationDataService } from '../../Service/authentication-data.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -11,23 +12,81 @@ import { FormsModule } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  username = 'javainuse'
-  password = '1234'
+  userNamePassword:UserNameAndPassword=new UserNameAndPassword();
+  //this.usernamePassword.userName;=username;
+  //this.usernamePassword.password;=password;
+  //username = 'javainuse'
+  //password = '1234'
   invalidLogin = false
-
-  constructor(private router: Router,
-    private loginservice: AuthenticationService) { }
+  
+  constructor(private router: Router, private authenticationDataService: AuthenticationDataService  ,private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
   }
 
-  checkLogin() {
-    if (this.loginservice.authenticate(this.username, this.password)
-    ) {
+  /*checkLogin() {
+    
+    if (this.loginservice.authenticate(this.usernamePassword)) {
       //this.router.navigate(['admin']);
       this.invalidLogin = false
     } else
       this.invalidLogin = true
   }
+*/
+  authenticate() {
+    this.authenticationDataService.getAuthenticationDetails(this.userNamePassword).subscribe(data => {
+      if (data == null){
+        window.alert('Incorrect Username or Password!');
+        this.invalidLogin = true;
+        return false;
+      }
+      else if (data.type == 'ch') {
+        //sessionStorage.setItem('username', this.userNamePassword.userName);
+        window.alert('Welcome Clearing House');
+        this.router.navigate(['clearing-house']);
+        this.invalidLogin = false;
+        return true;
+      }
+      else if (data.type == 'admin') {
+        //sessionStorage.setItem('username', this.userNamePassword.userName);
+        window.alert('Welcome Admin');
+        this.router.navigate(['admin']);
+        this.invalidLogin = false;
+        return true;
 
+      }
+
+      else if (data.type == 'cm') {
+
+        //sessionStorage.setItem('username', this.userNamePassword.userName);
+        window.alert('Welcome Member');
+        this.router.navigate(['clearing-member']);
+        this.invalidLogin = false;
+        return true;
+
+      }
+    
+
+    })
+
+  }
+}
+
+
+
+
+export class DBAccountReciever{
+    constructor(){}
+    clearingMemberId: Number;
+    clearingMemberName: String; //"CitiGroup",
+    userName: String;           //"citi",
+    password: String;            //"citilogin",
+    type: String;               //"cm"
+}
+
+export class UserNameAndPassword{
+  
+  userName:string;   //"citi";
+  password:string;   //"citilogin"; 
+  constructor (){ };
 }
