fef3d8da552e5d7d5cf84fe200df22a8285bd14f
namrataMahankal <namrata1997@gmail.com>
2020-10-01 11:17:15 +0530

    matrix updated

--- src/app/Components/ClearingHouse/clearinghouse.component.ts
+++ src/app/Components/ClearingHouse/clearinghouse.component.ts
@@ -67,7 +67,7 @@ export class ClearingHouseComponent {
   str;
 
 obMatrixData:Array<any>;
-  obCols:string[]=['CM','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'];
+  obCols:string[]=['CM','1','2','3','4','5','6','7','8','9'];
   oBMatrixData:ObligationMatrix[];
   panelOpenState = false;
   // obligation report panel
